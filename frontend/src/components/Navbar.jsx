7ac82d4fc8827baf065dfe92b0220567b5b67a79
Adlan Khaliev <78567323+khaliev@users.noreply.github.com>
2024-07-07 10:30:30 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <nav className="NavbarItems">
       <div className="navbarLogo">
-        <h1>TravelON</h1>
+        <h1>TravelOn</h1>
       </div>
       <ul className="navbarMenu">
         <li>Home</li>
